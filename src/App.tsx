1ba7e584f60cb5ce5c1c98e7902e41b18e30e2d8
amr mersal <amral3117@gmail.com>
2026-04-29 17:29:57 +0300

    l

--- src/App.tsx
+++ src/App.tsx
@@ -32,9 +32,10 @@ import HospitalityAboutPage from "./pages/HospitalityAboutPage";
 import HospitalityCareersPage from "./pages/HospitalityCareersPage";
 import HospitalityInsightsPage from "./pages/HospitalityInsightsPage";
 import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
+import TermsConditionsPage from './pages/TermsConditionsPage';
 import NotFound from "./pages/NotFound";
 import ChatAgent from "./components/ChatAgent";
-import TermsConditionsPage from './pages/TermsConditionsPage';
+
 
 const queryClient = new QueryClient();
 
